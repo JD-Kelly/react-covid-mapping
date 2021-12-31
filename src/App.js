de16c16c740fa46cc47ec06e4fe885cafcd33855
Jonny Kelly <jonny.kelly192@gmail.com>
2021-12-31 12:50:05 +0000

    function to process data from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { coordinates } from "./country_coordinates"
 import axios from "axios";
 
 import './App.css';
@@ -21,11 +22,45 @@ class App extends Component{
         method:"get",
         url: "https://corona-api.com/countries",
       });
-      console.log(response.data.data);
+      const countries_data = this.processData(response.data.data);
+      this.setState({
+        countries_data,
+        data_loaded: true
+      })
     } catch (e) {
       console.log("unable to retrieve data", e);
     }
   }
+
+  processData = (data) => {
+    let processed = [];
+
+    for (const d of data) {
+      let obj = {
+        name: d.name,
+        code: d.code, 
+        updated_at: d.updated_at,
+        confirmed: d.latest_data.confirmed,
+        deaths: d.latest_data.deaths,
+        recovered: d.latest_data.recovered,
+      };
+      
+// patch for country co-ordinates
+obj['coordinates'] = {
+  latitude:
+    coordinates.find(f => f.country_code === d.code) !== undefined ? 
+    coordinates.find(f => f.country_code === d.code).latlng[0] : 0,
+  longitude:
+    coordinates.find(f => f.country_code === d.code) !== undefined ? 
+    coordinates.find(f => f.country_code === d.code).latlng[1] : 0,
+    }
+    processed.push(obj);
+  }
+
+    return processed;
+};
+
+  
 
   render() {
     const { countries_data, data_loaded } = this.state;
@@ -35,8 +70,8 @@ class App extends Component{
       </div>
     ) : null;
   }
-}
 
+}
 
 
 
